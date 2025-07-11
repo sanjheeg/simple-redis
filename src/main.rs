f24352a2aef5dfccedb32996a9f41eb2a291e43c
sanjheeg <gupta.sanjhee@gmail.com>
2025-07-10 22:14:04 -0400

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    
+fn main() {    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
